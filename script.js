8a575532a9f89b18aad06804aed83f4f0c02fc86
Ellina Lapina <ellina_la@yahoo.com>
2018-03-08 21:40:26 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+// Step 1
+//var someMonth;
+//function theMonth; 		
+//currentMonth;		
+//var summerMonth; 		
+//myLibraryAFunction;
+
+// Step 2
+//10 				
+//"My name is Ellina" 			
+//true 				
+//null
+
+// Step 3
+//var a1 = 3 * 5 + 3 * 8;
+//var a2 = a1 + 40;
+
+// Step 4
+//var strFirstName;
+//var strLastName;
+//var strAddress;
+//var strCity;
+//var strState;
+//var liZip Code;
+//var liYour Age;
+//var strReferralSource;
+//var strMayWeContactYou;
+
+// Step 5
+//var strFirstName;
+//strFirstName = "Ellina";
+
+//var strLastName;
+//strLastName = "Lapina";
+
+//var strAddress;
+//strAddress = "1000 Balboa Ave."
+
+//var strCity = "San Diego";
+
+//var strState = "CA";
+
+//var liZip Code = 91011;
+
+//var liYour Age = 25, strReferralSource = "Social Media", strMayWeContactYou = true;
+
+// Step 6
+//var age = 25;
+//window.console.log("I am " + age + " years old.");
+
+//var compare = 100 < 1000;
+//window.console.log("It is " + compare + " that 100 is less than 1000.");
+
+//var answer = false;
+//window.console.log("The answer is " + answer + ".");
+
+// Step 7
+//var month = "September";
+//window.console.log("I was born on " + month + " " + year + ".");
+//var year = "1992";
+
+// Step 8
+//var someString = "\'Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\"\'";
+//window.console.log(someString);
+
+// Step 9
+//var a1 = null;
+//window.console.log(a1);
+
+//var a2;
+//window.console.log(a2);
+
+// Step 10
+//window.console.log(typeof "Name");
+//window.console.log(typeof 33);
+//window.console.log(typeof true);
+//window.console.log(typeof null);
+//window.console.log(typeof month);
+
+// Step 11
+//window.alert("Hello Zak Ruvalcaba" + " , welcome to the JavaScript class!");
+
+// Step 12
+//var name = "Ellina Lapina";
+//window.alert("Hello " + name + " , welcome to the JavaScript class!");
+
+//Step 13
+//var name = "Ellina Lapina", course = "Java Script"
+//window.alert("Hello " + name + " , welcome to the " + course + " class!");
+
+//Step 14
+//window.alert("Hello Zak Ruvalcaba! \nWelcome to the JavaScript class!");
+
+//Step 15
+//var name = window.prompt("What is your name?");
+//window.alert("Hello " + name + " , welcome to the JavaScript class!");
+
+//Step 16
+//var name = window.prompt("What is your name?");
+//var course = window.prompt("What course are you taking?");
+//window.alert("Hello " + name + " , welcome to the " + course + " class!");
+
+//Step 17
+//x = 10;
+//y = 20;
+//window.console.log(x+y);
+
+//Step 18
+//var x = 20;
+//x+=20;
+//window.console.log(x);
+
+//Step 19
+//var x = 20;
+//x*=5;
+//window.console.log(x);
+
+//Step 20
+//var x = 20%3;
+//x/=1
+//window.console.log(x);
+
+//Step 21
+//var x = (5>4)||(7>10);
+//window.console.log(x);
+
+//Step 22
+//var x = (5==4)&&(7<10);
+//window.console.log(x);
+
+//Step 23
+//var widget = new Date();
+//window.console.log(typeof widget);
+
+//Step 24
+//var widget = new Date();
+//window.console.log(widget instanceof Date);
+
+//Step 25
+//var widget = new Date();
+//window.console.log( !(widget instanceof Date) );
+
+
+
